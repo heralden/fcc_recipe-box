ca80cd8700592d4ea01212af6a0fdcfa43c0942d
Kevin <regen@anche.no>
2017-12-16 18:33:11 +0100

    Use enzyme for additional tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+import Recipe from './Recipe';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('should add a new recipe when clicking the button', () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find('button');
+  button.simulate('click');
+
+  expect(wrapper.find(Recipe)).toHaveLength(2);
+});
+
+it('should update an existing recipe', () => {
+  const wrapper = mount(<App />);
+  const div = wrapper.find('#Recipe-1');
+  div.simulate('click');
+  const textarea = wrapper.find('textarea');
+  textarea.simulate('change', { target: { value: "foobar" } });
+  const button = wrapper.find('.Icon-button').first();
+  button.simulate('click');
+
+  expect(wrapper.find('p').at(1).text()).toBe("foobar");
+});
+
+it('should delete an existing recipe', () => {
+  const wrapper = mount(<App />);
+  const div = wrapper.find('#Recipe-1');
+  div.simulate('click');
+  const button = wrapper.find('.Icon-button').at(2);
+  button.simulate('click');
+
+  expect(wrapper.find(Recipe)).toHaveLength(1);
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,10 @@
+// Enzyme
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+// localStorage
 class LocalStorageMock {
   constructor() {
     this.store = {};
